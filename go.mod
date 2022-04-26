4b47b3516c2a5f770fbf3db3e8bd524e4a0379bf
Nic Jackson <jackson.nic@gmail.com>
2022-04-26 10:37:04 +0100

    Scaffold mocks for Waypoint client, write example test

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -1,20 +1,22 @@
 package client
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
+	gen "github.com/hashicorp-dev-advocates/waypoint-client/pkg/waypoint"
+	"github.com/hashicorp-dev-advocates/waypoint-client/pkg/waypoint/mocks"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
-//func setup() {
-//	ws := pbmocks.WaypointServer{}
-//
-//	restartCh := make(chan struct{})
-//	client := TestServer(t, m,
-//		TestWithContext(ctx),
-//		TestWithRestart(restartCh),
-//	)
-//}
+func setupTests(t *testing.T) (*waypointImpl, *mocks.WaypointClient) {
+	grpcMock := &mocks.WaypointClient{}
+	client := &waypointImpl{client: grpcMock}
+
+	return client, grpcMock
+}
 
 func TestReturnsDefaultConfig(t *testing.T) {
 	c := DefaultConfig()
@@ -23,4 +25,30 @@ func TestReturnsDefaultConfig(t *testing.T) {
 	require.Equal(t, "", c.Token)
 	require.False(t, c.UseInsecureSkipVerify)
 	require.Nil(t, c.TLSConfig)
+}
+
+func TestGetVersionInfoReturnsInfo(t *testing.T) {
+	// setup the client with a mock grpc client
+	c, m := setupTests(t)
+
+	// setup the mock call to return the version
+	gvr := &gen.GetVersionInfoResponse{Info: &gen.VersionInfo{}}
+	m.On("GetVersionInfo", mock.Anything, mock.Anything, mock.Anything).Return(gvr, nil)
+
+	vi, err := c.GetVersionInfo(context.TODO())
+	require.NoError(t, err)
+
+	// check that the function returns the info from the GetVersionInfo call
+	require.Equal(t, gvr.Info, vi)
+}
+
+func TestGetVersionInfoReturnsError(t *testing.T) {
+	// setup the client with a mock grpc client
+	c, m := setupTests(t)
+
+	// setup the mock call to return the error
+	m.On("GetVersionInfo", mock.Anything, mock.Anything, mock.Anything).Return(nil, fmt.Errorf("unable to make call"))
+
+	_, err := c.GetVersionInfo(context.TODO())
+	require.Error(t, err)
 }
